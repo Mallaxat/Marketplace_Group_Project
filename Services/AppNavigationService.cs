df66e6f6098ed4907c09ee88684b85f3075879f7
banan1245 <mkazaev128@gmail.com>
2026-09-17 14:48:02 +0500

    Написаны файлики

---
+++ Services/AppNavigationService.cs
@@ -0,0 +1,85 @@
+﻿using Marketplace_Group_Project.Models;
+using Marketplace_Group_Project.ViewModels;
+using Marketplace_Group_Project.Views;
+using System.Linq;
+using System.Windows;
+
+namespace Marketplace_Group_Project.Services
+{
+    /// <summary>
+    /// Отвечает за переходы между окнами приложения
+    /// и хранит текущего авторизованного пользователя.
+    /// </summary>
+    public class AppNavigationService
+    {
+        private readonly MarketplaceService _marketplaceService;
+
+        public User? CurrentUser { get; private set; }
+
+        public AppNavigationService(MarketplaceService marketplaceService)
+        {
+            _marketplaceService = marketplaceService;
+        }
+
+        /// <summary>
+        /// Открывает главное окно покупателя.
+        /// </summary>
+        public void NavigateToUserMain(User user)
+        {
+            CurrentUser = user;
+
+            var viewModel = new UserMainViewModel(_marketplaceService, this, user);
+            var window = new UserMainWindow
+            {
+                DataContext = viewModel
+            };
+
+            OpenWindow(window);
+        }
+
+        /// <summary>
+        /// Открывает главное окно администратора.
+        /// </summary>
+        public void NavigateToAdminMain(User user)
+        {
+            CurrentUser = user;
+
+            var viewModel = new AdminMainViewModel(_marketplaceService, this, user);
+            var window = new AdminMainWindow
+            {
+                DataContext = viewModel
+            };
+
+            OpenWindow(window);
+        }
+
+        /// <summary>
+        /// Возвращает пользователя на окно входа.
+        /// </summary>
+        public void Logout()
+        {
+            CurrentUser = null;
+
+            var loginWindow = new MainWindow();
+            OpenWindow(loginWindow);
+        }
+
+        /// <summary>
+        /// Закрывает все открытые окна, кроме нового, и показывает новое.
+        /// </summary>
+        private static void OpenWindow(Window newWindow)
+        {
+            var oldWindows = Application.Current.Windows
+                .Cast<Window>()
+                .Where(w => w != newWindow)
+                .ToList();
+
+            foreach (var window in oldWindows)
+            {
+                window.Close();
+            }
+
+            newWindow.Show();
+        }
+    }
+}
---
+++ ViewModels/AdminMainViewModel.cs
@@ -0,0 +1,123 @@
+﻿using Marketplace_Group_Project.Models;
+using Marketplace_Group_Project.Services;
+using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace Marketplace_Group_Project.ViewModels
+{
+    /// <summary>
+    /// ViewModel главного окна администратора.
+    /// Отвечает за управление товарами и заказами.
+    /// </summary>
+    public class AdminMainViewModel : ViewModelBase
+    {
+        private readonly MarketplaceService _marketplaceService;
+        private readonly AppNavigationService _navigationService;
+        private readonly User _currentUser;
+
+        private Product? _selectedProduct;
+        private Order? _selectedOrder;
+        private StatusEnum _selectedStatus;
+
+        public AdminMainViewModel(MarketplaceService marketplaceService,
+                                  AppNavigationService navigationService,
+                                  User currentUser)
+        {
+            _marketplaceService = marketplaceService;
+            _navigationService = navigationService;
+            _currentUser = currentUser;
+
+            Products = new ObservableCollection<Product>();
+            Orders = new ObservableCollection<Order>();
+            Statuses = new ObservableCollection<StatusEnum>();
+
+            foreach (StatusEnum status in Enum.GetValues(typeof(StatusEnum)))
+            {
+                Statuses.Add(status);
+            }
+
+            RefreshProductsCommand = new RelayCommand(_ => LoadProducts());
+            DeactivateProductCommand = new RelayCommand(_ => DeactivateProduct(), _ => SelectedProduct != null);
+            RefreshOrdersCommand = new RelayCommand(_ => LoadOrders());
+            ChangeOrderStatusCommand = new RelayCommand(_ => ChangeOrderStatus(), _ => SelectedOrder != null);
+            LogoutCommand = new RelayCommand(_ => Logout());
+
+            LoadProducts();
+            LoadOrders();
+        }
+
+        public ObservableCollection<Product> Products { get; }
+        public ObservableCollection<Order> Orders { get; }
+        public ObservableCollection<StatusEnum> Statuses { get; }
+
+        public string CurrentUserName => _currentUser.Login;
+
+        public Product? SelectedProduct
+        {
+            get => _selectedProduct;
+            set => SetProperty(ref _selectedProduct, value);
+        }
+
+        public Order? SelectedOrder
+        {
+            get => _selectedOrder;
+            set => SetProperty(ref _selectedOrder, value);
+        }
+
+        public StatusEnum SelectedStatus
+        {
+            get => _selectedStatus;
+            set => SetProperty(ref _selectedStatus, value);
+        }
+
+        public ICommand RefreshProductsCommand { get; }
+        public ICommand DeactivateProductCommand { get; }
+        public ICommand RefreshOrdersCommand { get; }
+        public ICommand ChangeOrderStatusCommand { get; }
+        public ICommand LogoutCommand { get; }
+
+        private void LoadProducts()
+        {
+            Products.Clear();
+            var products = _marketplaceService.GetProducts();
+            foreach (var product in products)
+            {
+                Products.Add(product);
+            }
+        }
+
+        private void DeactivateProduct()
+        {
+            if (SelectedProduct == null)
+                return;
+
+            _marketplaceService.DeactivateProduct(SelectedProduct.Id);
+            LoadProducts();
+        }
+
+        private void LoadOrders()
+        {
+            Orders.Clear();
+            var orders = _marketplaceService.GetUserOrders(_currentUser.Id);
+            foreach (var order in orders)
+            {
+                Orders.Add(order);
+            }
+        }
+
+        private void ChangeOrderStatus()
+        {
+            if (SelectedOrder == null)
+                return;
+
+            _marketplaceService.ChangeOrderStatus(SelectedOrder, SelectedStatus);
+            LoadOrders();
+        }
+
+        private void Logout()
+        {
+            _navigationService.Logout();
+        }
+    }
+}
