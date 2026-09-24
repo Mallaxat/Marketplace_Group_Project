eb631e76ab9e24a6db08f9cfebed414bdd39c3af
Alina <alina-lapkina@mail.ru>
2026-09-24 13:59:40 +0300

    Слитая ветка со всеми для тестов

--- Models/MarketplaceDbContext.cs
+++ Models/MarketplaceDbContext.cs
@@ -20,7 +20,7 @@ namespace Marketplace_Group_Project.Models
 
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
-			optionsBuilder.UseSqlServer($@"Server=(localdb)\mssqllocaldb;Database=MarketplaceDB;Trusted_Connection=True;")
+			optionsBuilder.UseSqlServer(@"Server=localhost;Database=MarketplaceDB;Trusted_Connection=True;TrustServerCertificate=True;")
 				.UseSeeding((context, _) =>
 				{
 					// Этот код выполнится после Migrate(), если данных ещё нет
